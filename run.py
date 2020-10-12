cf281bac1d8547a1a5893a0ccec76f4845e70dbc
annalisa-f <nalif6342@gmil.com>
2020-10-12 20:16:06 +1300

    Sit down #2
    Backgrounds now change

--- run.py
+++ run.py
@@ -5,21 +5,24 @@ WEATHERDB = 'weather.db'
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET'])
+@app.route('/')
 def index():
-    continent = request.args.get('cont')
-    print(continent)
+
     return render_template ('index.html')
 
 
 
-@app.route('/cont', methods = ['GET'])
-def set_continent():
+@app.route('/continent', methods=['POST'])
+def scores_list():
 
-    print(continent)
-    #con = sqlite3.connect(WEATHERDB)
-    #cur = con.execute('SELECT imglink FROM continents WHERE id =?', continent)
-    #imgthingy = cur
-    #con.close()
+    entry = request.json
 
-    return jsonify(continent)
+    con = sqlite3.connect(WEATHERDB)
+    cur = con.execute('SELECT imglink FROM continents WHERE id = ?', [entry])
+
+    imgUrl = cur.fetchone()
+
+    con.close()
+
+
+    return jsonify(imgUrl)
--- static/js.js
+++ static/js.js
@@ -1,2 +1,24 @@
-fetch('/cont', { method: 'GET' })
-  .then(response => console.log(response))
+
+
+function changeBackground(imgUrl) {
+  document.body.style.backgroundImage = "url('static/maps/" + imgUrl + "')";
+}
+
+
+document.querySelectorAll('.cont').forEach(item => {
+  item.addEventListener('click', event => {
+
+    var whichCont = event.target;
+    let contNum = whichCont.id
+
+    fetch('/continent', {
+      method: 'POST',
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify(contNum)
+    })
+    .then(response => response.json())
+    .then(data => {
+      changeBackground(data[0])
+    })
+  })
+})
